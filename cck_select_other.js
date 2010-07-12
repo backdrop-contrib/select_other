5e201b081299f2b689e9db4bfe59da03bc179c17
Matthew Radcliffe <mradcliffe@softpixel.com>
2010-07-12 19:25:34 +0000

    * Bug fix: [#848102]

--- cck_select_other.js
+++ cck_select_other.js
@@ -10,7 +10,7 @@ Drupal.behaviors.cckSelectOther = function (context) {
 
 //  $.browser.msie == true ? $(this).click(Drupal.ConditionalFields.fieldChange) : $(this).change(Drupal.ConditionalFields.fieldChange); 
 
-  $.browser.msie == true ? $(this).click(cckSelectOther.switch) : $(this).change(cckSelectOther.switch);
+  $.browser.msie == true ? $(this).click(cckSelectOther.switchField) : $(this).change(cckSelectOther.switchField);
 
 //  document.write(Drupal.settings.CCKSelectOther.field.length);
   var field_str = new String(Drupal.settings.CCKSelectOther.field);
@@ -32,7 +32,7 @@ Drupal.behaviors.cckSelectOther = function (context) {
 
 }
 
-cckSelectOther.switch = function () {
+cckSelectOther.switchField = function () {
   var field_str = new String(Drupal.settings.CCKSelectOther.field);
   var fields = new Array();
   fields = field_str.split(',');
