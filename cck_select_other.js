1620f62e08fbd9b5e80a009ed92bca9ea5700a85
Matthew Radcliffe <mradcliffe@softpixel.com>
2013-07-13 19:00:56 -0400

    Issue #2041453 by mradcliffe: Rewrite widget to improve array parent functionality (field_collection) and add support for list_integer and list_float.

--- cck_select_other.js
+++ cck_select_other.js
@@ -1,39 +1,42 @@
-//$Id$
 /**
  * @file
- * cck_select_other javascript file 
+ * CCK Select Other Javascript Behaviors
  */
 
 (function ($) {
   Drupal.behaviors.cckSelectOther = {
+    /**
+     * Bind to each cck select other field widget delta select element.
+     */
     attach: function (context, settings) {
-
       // Prevent errors
       if (typeof settings.CCKSelectOther != 'object') return;
 
-      $.each(settings.CCKSelectOther, function(n,MyCCKSelectOther){
+      var ActionBind = (($.browser.msie == true) ? 'click' : 'change');
+      var input_element = '';
 
-        // Prevent errors
-        if (typeof MyCCKSelectOther.field_id == 'undefined') return;
+      for (n in settings.CCKSelectOther) {
+        // Bind to each field list element. This could probably be more
+        // optimized instead of binding for each?
+        if (typeof settings.CCKSelectOther[n].list_element != 'string') continue;
+        if (typeof settings.CCKSelectOther[n].input_element != 'string') continue;
 
-        var field_id = new String(MyCCKSelectOther.field_id);
-        var select_id = new String(field_id + '-select-other-list');
-        var text_id = new String(field_id + '-select-other-text-input');
+        $('#' + settings.CCKSelectOther[n].list_element).bind(ActionBind, {element: settings.CCKSelectOther[n].input_element},  this.toggle).trigger(ActionBind);
+      }
+    },
+    /**
+     * Look through selected options of select list, and toggle the display
+     * based on whether or not other is selected.
+     */
+    toggle: function (e) {
+      var input_element = e.data.element;
+      var select_other = 'none';
 
-        var ActionBind = (($.browser.msie == true) ? 'click' : 'change');
-
-        $(document).ready( function() {
-          // We need to go up further up the element chain to work around 'add another item'
-          $('select#edit-' + select_id).bind(ActionBind,function() {
-            // Add parent() to hide input wrapper
-            var selected_other = 'none';
-            $(this).children(':selected').each(function() {
-              selected_other = ($(this).val() == 'other') ? 'block' : 'none';
-            });
-            $('input#edit-' + text_id).parent().css('display', selected_other);
-          }).trigger(ActionBind);
-        });
+      $(this).children(':selected').each(function() {
+          selected_other = ($(this).val() == 'other') ? 'block' : 'none';
       });
+
+      $('#' + input_element).parent().css('display', selected_other);
     }
   }
 })(jQuery);
